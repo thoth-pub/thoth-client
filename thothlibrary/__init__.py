1d77b9ec312a08d768f89a317788150f78361d68
Javier Arias <javier@jarias.org>
2023-01-16 10:26:19 +0000

    Bump v0.17.0

--- thothlibrary/__init__.py
+++ thothlibrary/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """GraphQL client for Thoth"""
 
-__version__ = "0.16.0"
+__version__ = "0.17.0"
 __author__ = "Javier Arias <javi@openbookpublishers.com>"
 __copyright__ = "Copyright (c) 2020 Open Book Publishers"
 __license__ = "Apache 2.0"
